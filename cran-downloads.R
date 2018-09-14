fe9fd5f5bbcbd190dd1fa0152b8a8688a7f608ec
Adam Deacon <adam@deaconsworld.org.uk>
2018-09-14 09:24:12 +0100

    Added height/width for api_create

--- cran-downloads.R
+++ cran-downloads.R
@@ -14,10 +14,11 @@ p <- ggplot(data = stats, aes(x = date, y = count, group=package, color=package)
   scale_color_discrete(name = "Package") +
   scale_y_continuous(trans = "log10")
 
-g <- ggplotly(p, tooltip = c("x","y","colour")) %>%
+g <- ggplotly(p, tooltip = c("x","y","colour"), height = 450, width = 720) %>%
   layout(title = "Package Downloads",
          #hovermode = 'compare',
-         legend = (list(orientation = "v")
-         ))
+         legend = (list(orientation = "v"))
+         ) 
 
 print(g)
+# api_create(g, filename = "cran-downloads")
