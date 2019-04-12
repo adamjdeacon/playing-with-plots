7d9818ba45be8e479b9ed64199a2787b67c04d8d
Adam Deacon <adam@deaconsworld.org.uk>
2019-04-12 17:22:51 +0100

    Updated some stuff - No one really cares

--- jira-confluence.R
+++ jira-confluence.R
@@ -2,15 +2,16 @@ library(readr)
 library(ggplot2)
 
 d <- read_delim('./performance.txt', delim=" ", col_types = cols(
-  time = col_time(format = "%H:%M:%S")
+  time = col_character()
 ))
 
 d$cpu <- as.numeric(d$cpu)
+d$time <- as.POSIXct(paste0("2019-04-",d$day," ",d$time))
 
 g <- ggplot(data=d, aes(x=time, y=cpu, group=process, color=process)) +
   geom_line(size=0.5) + 
   scale_color_discrete(name="process") +
-  scale_x_time()
+  scale_x_datetime()
 
 print(g)
 
